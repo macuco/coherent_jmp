e7232aad883ee69dbb82cf22ed445ca1af93212c
macuco <macuco8@hotmail.com>
2023-01-18 10:11:08 -0600

    started project

---
+++ src/test/java/com/coherent/test/ReservationsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.coherent.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		if(true == true);
+	}
+
+}
